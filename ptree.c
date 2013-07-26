d68b4090d007be378a713420c2e53356a656872e
TangLin <john-200000000@163.com>
2013-07-26 15:23:22 +0800

    Finish the system call which can print the infomation of the process tree

--- ptree.c
+++ ptree.c
@@ -1,11 +1,9 @@
 #include "ptree.h"
 
 SYSCALL_DEFINE2(ptree , struct prinfo , *buf , int , *nr){
-	int fb;//Feedback
-	int snr;//nr for system
-	struct prinfo *sbuf;//buf for system
+	int fb;
+	struct prinfo *sbuf;
 	int lock=0;
-	//Initial system call
 	if(!buf || !nr || *nr<1){
 		return -EINVAL;
 	}	
@@ -22,7 +20,7 @@ SYSCALL_DEFINE2(ptree , struct prinfo , *buf , int , *nr){
 		return -EFAULT;
 	}
 	auto_lock(&lock);
-	fb = process_tree(sbuf,&snr);
+	fb = process_tree(sbuf);
 	auto_lock(&lock);
 	if(!fb){
 		return false;
@@ -40,11 +38,13 @@ SYSCALL_DEFINE2(ptree , struct prinfo , *buf , int , *nr){
 	return fb;
 }
 
-int process_tree(struct prinfo *sbuf,int *snr){
-	printk(KERN_INFO "PROCESS TREE BEGIN...\n");
-	struct task_struct *cur = get_init_task();
+int process_tree(struct prinfo *sbuf){
+	int counter = 0;
+	printk(KERN_INFO "PROCESS TREE BEGIN...[SNR = %d]\n",snr);
+	struct task_struct *root = get_init_task();
+	dfs_recu_travel(root,sbuf,&counter);
 	printk(KERN_INFO "PROCESS TREE OVER..\n");
-	return 1;
+	return counter;
 }
 
 struct task_struct *get_init_task(void){
@@ -65,14 +65,12 @@ int has_children(struct task_struct *task){
 	
 int has_sibling(struct task_struct *task){
 	struct list_head *head= &task -> parent -> children;
-	if(!task){
-		return false;
-	}
 	if(list_is_last(&task -> sibling,head)){
 		return false;
 	}
 	return true;
 }
+
 int is_a_process(struct task_struct *task){
 	if(!thread_group_empty(task)){
 		if(!thread_group_leader(task)){
@@ -94,12 +92,72 @@ void auto_lock(int *lock){
 	return;
 }
 
-void process_node(struct prinfo *buf,struct task_struct *task){
+void process_node(struct task_struct *task,struct prinfo *sbuf,int *counter){
+    if(!is_a_process(task)) 
+      return;
+    printk(KERN_INFO"------------[task]------------: \n[PID]=%d\n[NAME]=%s\n[COUNTER]=%d\n",task -> pid,task->comm,*counter);
+		struct prinfo new_node;
+		struct task_struct *first_child;
+		struct task_struct *first_sibling;
+		init_node(&new_node,task);	
+		if(has_children(task)){
+			first_child = list_entry(
+					task -> children.next,
+					struct task_struct,
+					sibling
+					);
+			new_node.first_child_pid = first_child -> pid;
+		}
+		if(has_sibling(task)){
+			first_sibling = list_entry(
+					task -> sibling.next,
+					struct task_struct,
+					sibling	
+					);
+			new_node.next_sibling_pid = first_sibling -> pid;
+		}
+		new_node.uid = task_uid(task);	
+		strncpy(sbuf -> comm, task -> comm,64);
+		sbuf[(*counter)+1] = new_node; 		
+		(*counter) ++;	
 }
 
-struct task_struct *get_next_node(struct task_struct *cur){
-	struct task_struct *node = cur;
-	return node;
+void dfs_recu_travel(struct task_struct *_cur,struct prinfo *sbuf,int *counter){
+  if((*counter) > 1 && _cur -> pid ==0)
+    return;
+	if((*counter)>snr){
+		return;
+  }
+  struct task_struct *cur = _cur;
+	process_node(cur,sbuf,counter);
+	if(has_children(cur)){
+		struct task_struct *first_child = list_entry(
+							                                cur -> children.next,
+							                                struct task_struct,
+							                                sibling
+						                            );
+		dfs_recu_travel(first_child,sbuf,counter);
+	}
+	if(has_sibling(cur)){
+	  struct task_struct *next_sibling = list_entry(
+		  cur -> sibling.next,
+		  struct task_struct,
+		  sibling
+		  );
+	  dfs_recu_travel(next_sibling,sbuf,counter);
+	}
+	printk(KERN_INFO"DFS_RECU_TRAVEL OVER ...\n");
+	
+}
+
+void init_node(struct prinfo *new_node,struct task_struct *task){
+	new_node -> state = task -> state;
+	new_node -> pid = task -> pid;
+	new_node -> parent_pid = task -> parent -> pid;
+	new_node -> first_child_pid = 0;
+	new_node -> next_sibling_pid = 0;
+	new_node -> uid = 0;
+	printk(KERN_INFO"INIT NODE...\n");
 }
 
 
--- ptree.h
+++ ptree.h
@@ -7,12 +7,20 @@
 #include <asm-generic/errno-base.h>
 #include <linux/stddef.h> /* for true and false */
 #include <asm/uaccess.h>
-int process_tree(struct prinfo *sbuf,int *snr);
+
+int snr;
+
+
+/*
+**function
+*/
+int process_tree(struct prinfo *sbuf);
 int has_children(struct task_struct *task);
 int has_sibling(struct task_struct *task);
 void auto_lock(int *lock);
 struct task_struct *get_init_task(void);
-void process_node(struct task_struct *task,struct prinfo * buf);
-struct task_struct *get_next_node(struct task_struct *cur);
+void process_node(struct task_struct *task,struct prinfo *sbuf,int *counter);
 int is_a_process(struct task_struct *task);
+void init_node(struct prinfo *new_node , struct task_struct *task);
+void dfs_recu_travel(struct task_struct *cur,struct prinfo *sbuf,int *counter);
 #endif
